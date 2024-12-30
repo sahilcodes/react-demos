70164689915d752be35b721a25896d51b6fed377
sahilcodes <sahilcancode@gmail.com>
2024-12-30 22:08:59 +0530

    Used UseEffect hook and counter on Getting advice

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MainContent = () => {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
 
     setAdvice(data.slip.advice);
+    setCount((counter) => counter + 1);
   }
 
-  getAdvice();
+  useEffect(function () {
+    getAdvice();
+  }, []);
 
   return (
     <main>
       <h2>{advice}</h2>
       <button onClick={getAdvice}>Get another advice</button>
+
+      <p>
+        You have read <strong>{count}</strong> pieces of advice.
+      </p>
     </main>
   );
 };
